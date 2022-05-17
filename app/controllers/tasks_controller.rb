04caa6a603a0a91dc8cd39fbbb10ae495301528c
carson mulligan <carsonmulligan45@gmail.com>
2022-05-17 15:08:45 +0100

    FEAT: back button created

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,4 +10,21 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+end
+
+
+private
+
+def set_task
+  @task = Task.find(params[:id])
+end
+
+def task_params
+  params.require(:task).permit(:title, :details, :completed)
 end
