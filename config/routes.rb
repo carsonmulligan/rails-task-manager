81d55591781a1e36f1f36251c8d54ec37fa2ba76
carson mulligan <carsonmulligan45@gmail.com>
2022-05-17 14:50:27 +0100

    FEAT: banner padding added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'tasks/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
   get 'tasks', to: 'tasks#index'
   get 'tasks/:id', to: 'tasks#show', as: :task
+
 end
